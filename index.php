b616fc01595d8c65acc7d7b5ad422761e62b7137
Adam Miedema <armgitaar@yahoo.com>
2020-10-16 14:47:11 -0600

    cleaver to cleavr

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Cleaver - Server provisioning and deployments made easy for you</title>
+    <title>Cleavr - Server provisioning and deployments made easy for you</title>
     <link rel="stylesheet" href="./app.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
@@ -14,7 +14,7 @@
 <div id="app" class="flex min-h-screen m-auto flex-col">
     <div class="pb-16 flex flex-col justify-center w-full max-w-4xl mx-auto flex-grow">
         <div class="mx-auto">
-            <a href="https://cleaver.cloud">
+            <a href="https://cleavr.io">
                 <svg width="280" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                      viewBox="0 0 744 128">
                     <defs>
@@ -95,7 +95,7 @@
             </a>
         </div>
         <div class="mt-20 max-w-2xl mx-auto md:w-full flex flex-col md:flex-row self-center md:self-stretch md:justify-between text-lg uppercase text-gray-900 font-semibold">
-            <a href="https://cleaver.zendesk.com" target="_blank" class="hover:text-red-600 flex mt-4 align-middle">
+            <a href="https://cleavr.zendesk.com" target="_blank" class="hover:text-red-600 flex mt-4 align-middle">
                 <svg class="mr-2 text-gray-700 h-5 self-center" aria-hidden="true" focusable="false" role="img"
                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                     <g class="fa-group">
@@ -108,15 +108,15 @@
                 </svg>
                 Documentation
             </a>
-            <a href="https://twitter.com/getcleaver" target="_twitter" class="hover:text-red-600 flex mt-4">
+            <a href="https://twitter.com/thecleavr" target="_twitter" class="hover:text-red-600 flex mt-4">
                 <svg class="mr-2 text-gray-700 h-6 self-center" aria-hidden="true" focusable="false" role="img"
                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path fill="currentColor"
                           d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
                 </svg>
-                Cleaver
+                Cleavr
             </a>
-            <a href="https://forum.cleaver.cloud/"
+            <a href="https://forum.cleavr.io/"
                target="_slack" class="hover:text-red-600 flex mt-4">
                 <svg class="mr-2 text-gray-700 h-6 self-center" aria-hidden="true" focusable="false" role="img"
                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
@@ -124,7 +124,7 @@
                           d="M 16.109375 3 C 8.994375 3 3 8.7164375 3 15.773438 C 3 15.999438 3.0058594 29 3.0058594 29 L 16.109375 28.988281 C 23.230375 28.988281 29 23.051141 29 15.994141 C 29 8.9371406 23.230375 3 16.109375 3 z M 16 8 C 20.418 8 24 11.582 24 16 C 24 20.418 20.418 24 16 24 C 14.58 24 13.247797 23.625563 12.091797 22.976562 L 8 24 L 9.1210938 20.070312 C 8.4130937 18.877312 8 17.489 8 16 C 8 11.582 11.582 8 16 8 z"></path>
                 </svg>
                 Community </a>
-                <a href="https://releases.cleaver.cloud/"
+                <a href="https://releases.cleavr.io"
                target="_slack" class="hover:text-red-600 flex mt-4">
                 <svg class="mr-2 text-gray-700 h-6 self-center" aria-hidden="true" focusable="false" role="img"
                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
